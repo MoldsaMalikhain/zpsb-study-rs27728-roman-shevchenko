d7aa0d57e55cb66639066852ac29ce6c48dfa138
Moldas Malikhain <kerch999@gmail.com>
2020-03-31 20:04:43 +0200

    update

--- c++/Forck/main.c
+++ c++/Forck/main.c
@@ -6,32 +6,29 @@
 #include <signal.h>
 int line = 0;
 int lines = 0;
-int spaceCheck(FILE *iFile, char *text){
-    unsigned short space;
-    size_t len = 0;
-    int line = 0;
-    ssize_t read;
-    while((read = getline(&text, &len, iFile) != -1)){
+int spaceCheck(FILE *iFile, int lineN, pid_t childPid){
+    if(lineN < lines){
+        unsigned short space;
+        size_t len = 0;
+        int line = 0;
+        ssize_t read;
+        char *text;
+        for (len; len < lineN;line++){
             *text = fgetc(iFile);
-            if(feof(iFile)) break;
-            for (size_t i = 0; i < strlen(text); i++)
-            {
-                if(text[i] == ' ') space++;
-            }
-            line ++;
-            printf("w linii: %d jest %d spacji\n", line, space);
-            space = 0;
-            //if(*text == ' ') space++;
-            //printf("Retrieved line of length %zu:\n", read);
-            printf("%s", text);
         }
+        for(len = 0; len < strlen(text); len ++){
+            if(text[len] == ' ') space ++;
+            else if (text[len] == '\0') break;
+        }
+        printf("proces: %d; w linii: %d jest: %d spacji", childPid, lineN, space);
+    }
     fclose(iFile);
+    return 0;
 }
 
 int main(int argc, char **argv){
     FILE *iFile;
     unsigned short i;
-    char *text;
     
     iFile = fopen("input.txt", "r");
     if(!iFile) perror("Error opening file");
@@ -47,9 +44,8 @@ int main(int argc, char **argv){
                 return 1;
             }
             else if(fo == 0){    
-                printf("proces: %d\t", getpid());
                 childPid = getpid();
-                spaceCheck(iFile, text);
+                spaceCheck(iFile, line, childPid);
                 return 0;
             }
             else{
@@ -59,7 +55,6 @@ int main(int argc, char **argv){
                 }else{
                     break;
                 }
-            printf("parent pid: %d\n", pid);
             }
         }
         
